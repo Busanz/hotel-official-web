0fe592ae93bf8e52449a84aaeb45b65369ed0595
Busanz <buddhisanz@gmail.com>
2026-04-04 18:54:17 +0200

    Fix mobile menu

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,8 +2,20 @@ import styles from './navbar.module.css';
 import Navlinks from '../Navlinks';
 import Logo from '../Logo';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { useState } from 'react';
 
 const Navbar = ({ setSelectPageOnNav, activePage }) => {
+  const [mobileActive, setMobileActive] = useState(false);
+
+  const handleClick = () => {
+    const scrolling = !mobileActive;
+    setMobileActive(scrolling);
+    const root = document.querySelector('#root');
+    scrolling && root
+      ? root.classList.add('stop_scrolling')
+      : root.classList.remove('stop_scrolling');
+  };
+
   return (
     <nav className={styles.navbar}>
       <Logo isOnFooter={false} setSelectLogo={setSelectPageOnNav} />
@@ -11,9 +23,14 @@ const Navbar = ({ setSelectPageOnNav, activePage }) => {
         isOnFooter={false}
         setSelectNavLink={setSelectPageOnNav}
         activePage={activePage}
-        className={styles.nav_links}
+        isMobileNavLinks={mobileActive}
+        setIsMobileNavLink={handleClick}
       />
-      <RxHamburgerMenu className={styles.mobile_menu} size={52} />
+      <RxHamburgerMenu
+        className={styles.mobile_menu}
+        size={52}
+        onClick={handleClick}
+      />
     </nav>
   );
 };
--- src/components/Navlinks/index.jsx
+++ src/components/Navlinks/index.jsx
@@ -1,17 +1,50 @@
+// import { useState } from 'react';
+
 import { convertFirtLetterCapital } from '../../utils/functions';
 import { menuLinks } from '../../data/data';
 import styles from './navlinks.module.css';
+import { IoCloseCircleOutline } from 'react-icons/io5';
 
-const Navlinks = ({ isOnFooter, setSelectNavLink, activePage }) => {
+const Navlinks = ({
+  isOnFooter,
+  setSelectNavLink,
+  activePage,
+  isMobileNavLinks,
+  setIsMobileNavLink,
+}) => {
+  const closeMobileMenu = () => {
+    if (isMobileNavLinks) {
+      setIsMobileNavLink(false);
+      window.scrollTo(0, 0);
+    }
+  };
+
+  const handleNavLinkClick = (menulink) => {
+    setSelectNavLink(menulink);
+    closeMobileMenu();
+  };
   return (
-    <div className={styles.navlinks}>
+    <div
+      className={`${styles.navlinks} ${isMobileNavLinks ? styles.mobile_menu : ''}`}
+      onClick={() => {
+        closeMobileMenu();
+        setSelectNavLink('home');
+      }}
+    >
+      {isMobileNavLinks && (
+        <>
+          <div className={styles.mobile_close}>
+            <IoCloseCircleOutline size={45} onClick={closeMobileMenu} />
+          </div>
+        </>
+      )}
       {menuLinks.map((menulink, index) => (
         <p
           className={`${isOnFooter ? styles.navlink_footer : styles.navlink} ${activePage === menulink ? styles.active : ''}`}
           key={index}
-          onClick={() => {
-            setSelectNavLink(menulink);
-            window.scrollTo(0, 0);
+          onClick={(e) => {
+            handleNavLinkClick(menulink);
+            e.stopPropagation();
           }}
         >
           {convertFirtLetterCapital(menulink)}
